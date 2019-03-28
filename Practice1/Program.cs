5fed2df61dffdf087a037aaf6c3454138dc282bc
Rustam Galeev <Vincent-cat@mail.ru>
2019-03-28 23:21:03 +0600

    Добавьте файлы проекта.

---
+++ Practice1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.	Написать приложение, которое выведет на экран Ваше имя и фамилию
+            string name, second_name;
+            name = "Рустам";
+            second_name = "Галеев";
+
+            Console.WriteLine("Практическое задание 1");
+            Console.WriteLine("Вас приветствует");
+            Console.WriteLine();
+            Console.WriteLine("Имя \t\t- " + name);
+            Console.WriteLine("Фамилия \t- " + second_name);
+
+            //2.	Написать приложение, которое ожидает ввода нескольких чисел и выводит на экран их сумму.
+
+            int cnt;
+            cnt = 1;
+            Console.WriteLine("Plese input " + cnt + "\t numer");
+            int x = int.Parse(Console.ReadLine());
+            cnt++;
+            Console.WriteLine("Plese input " + cnt + "\t numer");
+            int y = int.Parse(Console.ReadLine());
+            cnt++;
+            Console.WriteLine("Plese input " + cnt + "\t numer");
+            int z = int.Parse(Console.ReadLine());
+            cnt++;
+            Console.WriteLine("Plese input " + cnt + "\t numer");
+            int k = int.Parse(Console.ReadLine());
+            int sum = x + y + z + k;
+            Console.WriteLine("Sum of " + cnt + "\t numers is equal to \t" + sum);
+            Console.WriteLine("Good Luck");
+        }
+    }
+}
